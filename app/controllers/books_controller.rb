46798d7d4f8757f440886e199ebeab839b3280cc
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-05-18 07:30:06 +0000

    [add]部分テンプレート

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,8 @@
 class BooksController < ApplicationController
+   before_action :authenticate_user!,except: [:home, :about]
+
+   before_action :correct_user, only: [:edit, :update]
+
   def show
     @newbook = Book.new
     @book = Book.find(params[:id])
@@ -26,11 +30,6 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    if @book.user = current_user
-        render "edit"
-    else
-        redirect_to books_path
-    end
   end
 
   def update
