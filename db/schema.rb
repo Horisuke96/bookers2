c37dc102d723653a37e281353b4c881359377825
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-05-20 08:05:27 +0000

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_171926) do
+ActiveRecord::Schema.define(version: 2021_05_18_003619) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
@@ -31,9 +31,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_171926) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
-    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
